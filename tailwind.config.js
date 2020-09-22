d0b584f914988cb49fef428c6e29325b7fd2f068
Mark Fullbrook <mark.fullbrook@universalpacking.co.uk>
2020-09-22 23:00:09 +0100

    WIP

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,56 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-const uiColors = require('@tailwindcss/ui/colors')
+const defaultTheme = require("tailwindcss/defaultTheme")
+const uiColors = require("@tailwindcss/ui/colors")
 module.exports = {
-  purge: [],
+  purge: ["./resources/**/*.html", "./content/globals/settings.yaml"],
   theme: {
     screens: {
-        'sm': '400px',
-        'md': '768px',
-        'lg': '1024px',
-        'xl': '1280px',
+      sm: "400px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
     },
     extend: {
-        colors: {
-            yellow: {
-                ...uiColors.yellow,
-                default: '#fcfe75'
-            }
+      colors: {
+        yellow: {
+          ...uiColors.yellow,
+          default: "#fcfe75",
         },
-        fontFamily: theme => ({
-            sans: ['Inter var', ...defaultTheme.fontFamily.sans]
-        })
+        primary: "#fcfe75",
+      },
+      fontFamily: (theme) => ({
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      }),
     },
+    typography: (theme) => ({
+      default: {
+        css: {
+          color: theme("colors.gray.300"),
+          a: {
+            color: theme("colors.primary"),
+          },
+          strong: {
+            color: theme("colors.gray.100"),
+          },
+          h2: {
+            color: theme("colors.gray.100"),
+          },
+          h3: {
+            color: theme("colors.gray.100"),
+          },
+          h4: {
+            color: theme("colors.gray.100"),
+          },
+          blockquote: {
+            color: theme("colors.gray.200"),
+          },
+        },
+      },
+    }),
   },
   variants: {},
-  plugins: [
-      require('@tailwindcss/ui'),
-  ],
+  plugins: [require("@tailwindcss/ui")],
   future: {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
-  }
+  },
 }
